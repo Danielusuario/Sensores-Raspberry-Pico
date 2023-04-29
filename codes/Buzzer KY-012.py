3e9159ea8102fbdb2c01721baead9cb1adac266c
Beniez-paola <124212478+Beniez-paola@users.noreply.github.com>
2023-04-28 22:03:19 -0700

    Update Buzzer KY-012.py

--- codes/Buzzer KY-012.py
+++ codes/Buzzer KY-012.py
@@ -2,7 +2,16 @@
 #Benitez Solorzano Paola
 
 
+```python
+from machine import Pin, PWM
+from utime import sleep
 
+ky012 = PWM(Pin(0))
+ky012.freq(500)
+ky012.duty_u16(1000)
+sleep(1)
+ky012.duty_u16(0)
+```
 
 
 
