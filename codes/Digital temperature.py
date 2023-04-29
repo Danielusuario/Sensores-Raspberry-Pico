0fc91bfd3dc629ad19585bee11335f17d78dccf8
Beniez-paola <124212478+Beniez-paola@users.noreply.github.com>
2023-04-28 22:09:52 -0700

    Create Digital temperature.py

---
+++ codes/Digital temperature.py
@@ -0,0 +1,12 @@
+#Digital temperature KY-028
+#Benitez Solorzano Paola
+
+from machine import Pin
+import time,utime  
+
+tempDig=Pin(15,Pin.IN)  
+
+while True:
+    tempDig.value()
+    print(str(tempDig.value()) +"C")
+    utime.sleep(1)
